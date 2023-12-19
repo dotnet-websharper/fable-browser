053529141d26e7d82da4aa49f696580da1568426
Gyalus Gergő <gyalus.gergo.98@gmail.com>
2023-12-19 12:21:24 +0100

    - Added Inline tags to Ws.Browser.Blob fields, needs testing - Geolocation needs testing

--- src/ws-proxies/WebSharper.Browser.Blob/Proxies.fs
+++ src/ws-proxies/WebSharper.Browser.Blob/Proxies.fs
@@ -15,33 +15,51 @@ module private Proxies
 
     [<AbstractClass; Proxy(typeof<Browser.Types.BlobPropertyBag>)>]
     type BlobPropertyBag =
+        [<Inline>] 
         abstract ``type``: string with get, set
+        [<Inline>] 
         abstract endings: Browser.Types.BlobEndings with get, set
 
     [<AbstractClass; Stub; Proxy(typeof<Browser.Types.Blob>)>]
     type Blob =
+        [<Inline>] 
         abstract arrayBuffer: unit -> Promise<ArrayBuffer>
+        [<Inline>] 
         abstract size: int
+        [<Inline>] 
         abstract ``type``: string
+        [<Inline>] 
         abstract slice: ?start: int * ?``end``: int * ?contentType: string -> Blob
+        [<Inline>] 
         abstract text: unit -> Promise<string>
 
     [<Proxy(typeof<Browser.Types.BlobType>)>]
     type BlobType =
-        [<Inline("new Blob($1...)")>] abstract Create: ?blobParts: obj[] * ?options: BlobPropertyBag -> Blob
+        [<Inline("new Blob($blobParts, $options)")>] abstract Create: ?blobParts: obj[] * ?options: BlobPropertyBag -> Blob
     
     [<AbstractClass; Stub; Proxy(typeof<Browser.Types.FormData>)>]
     type FormDataProxy =
+        [<Inline>] 
         abstract append: name: string * value: string -> unit
+        [<Inline>] 
         abstract append: name: string * value: Blob * ?filename: string -> unit
+        [<Inline>] 
         abstract delete: name: string -> unit
+        [<Inline>] 
         abstract entries: unit -> (string * obj) seq
+        [<Inline>] 
         abstract get: name: string -> obj
+        [<Inline>] 
         abstract getAll: name: string -> obj[]
+        [<Inline>] 
         abstract has: name: string -> bool
+        [<Inline>] 
         abstract keys: unit -> string seq
+        [<Inline>] 
         abstract set: name: string * value: string -> unit
+        [<Inline>] 
         abstract set: name: string * value: Blob * ?filename: string -> unit
+        [<Inline>] 
         abstract values: unit -> obj seq
 
     [<Proxy(typeof<Browser.Types.FormDataType>)>]
--- src/ws-proxies/WebSharper.Browser.Geolocation/Proxies.fs
+++ src/ws-proxies/WebSharper.Browser.Geolocation/Proxies.fs
@@ -1,4 +1,4 @@
-module private Proxies
+module private rec Proxies
 open WebSharper
 open WebSharper.JavaScript
 
@@ -6,47 +6,80 @@ type WindowOrWorkerGlobalScope with
     [<Inline>] 
     static member Geolocation = Unchecked.defaultof<Browser.Types.Geolocation>
 
+[<Proxy(typeof<Browser.Types.Geolocation>)>]
 type GeolocationProxy =
-    abstract clearWatch: watchId: float -> unit
-    abstract getCurrentPosition: successCallback: (Browser.Types.Position->unit) * ?errorCallback: (Browser.Types.PositionError->unit) * ?options: Browser.Types.PositionOptions -> unit
-    abstract watchPosition: successCallback: (Browser.Types.Position->unit) * ?errorCallback: (Browser.Types.PositionError->unit) * ?options: Browser.Types.PositionOptions -> float
+    [<Inline>] abstract clearWatch: watchId: float -> unit
+    [<Inline>] abstract getCurrentPosition: successCallback: (Browser.Types.Position->unit) * ?errorCallback: (Browser.Types.PositionError->unit) * ?options: Browser.Types.PositionOptions -> unit
+    [<Inline>] abstract watchPosition: successCallback: (Browser.Types.Position->unit) * ?errorCallback: (Browser.Types.PositionError->unit) * ?options: Browser.Types.PositionOptions -> float
 
-// type Coordinates =
-//     /// Returns a double representing the position's latitude in decimal degrees.
-//     abstract latitude: float
-//     /// Returns a double representing the position's longitude in decimal degrees.
-//     abstract longitude: float
-//     /// Returns a double representing the position's altitude in meters, relative to sea level. This value can be null if the implementation cannot provide the data.
-//     abstract altitude: float option
-//     /// Returns a double representing the accuracy of the latitude and longitude properties, expressed in meters.
-//     abstract accuracy: float
-//     /// Returns a double representing the accuracy of the altitude expressed in meters. This value can be null.
-//     abstract altitudeAccuracy: float option
-//     /// Returns a double representing the direction in which the device is traveling. This value, specified in degrees, indicates how far off from heading true north the device is. 0 degrees represents true north, and the direction is determined clockwise (which means that east is 90 degrees and west is 270 degrees). If speed is 0, heading is NaN. If the device is unable to provide heading information, this value is null.
-//     abstract heading: float option
-//     /// Returns a double representing the velocity of the device in meters per second. This value can be null.
-//     abstract speed: float option
+    [<Inline>] 
+    default this.clearWatch(watchId) = (this |> As<Geolocation.Geolocation>).ClearWatch(int watchId)
+    [<Inline>] 
+    default this.getCurrentPosition(successCallback, errorCallback, options) = 
+        (this |> As<Geolocation.Geolocation>).GetCurrentPosition(new System.Action<_>(As >> successCallback),new System.Action<_>(As >> (Option.defaultValue ignore errorCallback)),options |> As)
+    [<Inline>] 
+    default this.watchPosition(successCallback, errorCallback, options) =
+        (this |> As<Geolocation.Geolocation>).WatchPosition(new System.Action<_>(As >> successCallback),new System.Action<_>(As >> (Option.defaultValue ignore errorCallback)),options |> As)
 
-// type Position =
-//     abstract coords: Coordinates
-//     abstract timestamp: float
+[<Proxy(typeof<Browser.Types.Coordinates>)>]
+type CoordinatesProxy =
+    /// Returns a double representing the position's latitude in decimal degrees.
+    [<Inline>] abstract latitude: float
+    /// Returns a double representing the position's longitude in decimal degrees.
+    [<Inline>] abstract longitude: float
+    /// Returns a double representing the position's altitude in meters, relative to sea level. This value can be null if the implementation cannot provide the data.
+    [<Inline>] abstract altitude: float option
+    /// Returns a double representing the accuracy of the latitude and longitude properties, expressed in meters.
+    [<Inline>] abstract accuracy: float
+    /// Returns a double representing the accuracy of the altitude expressed in meters. This value can be null.
+    [<Inline>] abstract altitudeAccuracy: float option
+    /// Returns a double representing the direction in which the device is traveling. This value, specified in degrees, indicates how far off from heading true north the device is. 0 degrees represents true north, and the direction is determined clockwise (which means that east is 90 degrees and west is 270 degrees). If speed is 0, heading is NaN. If the device is unable to provide heading information, this value is null.
+    [<Inline>] abstract heading: float option
+    /// Returns a double representing the velocity of the device in meters per second. This value can be null.
+    [<Inline>] abstract speed: float option
 
-// type PositionErrorCode =
-//     | PERMISSION_DENIED = 1
-//     | POSITION_UNAVAILABLE = 2
-//     | TIMEOUT = 3
+    [<Inline>] default this.latitude = (this |> As<Geolocation.Coordinates>).Latitude
+    [<Inline>] default this.longitude = (this |> As<Geolocation.Coordinates>).Longitude
+    [<Inline>] default this.altitude = Some (this |> As<Geolocation.Coordinates>).Altitude
+    [<Inline>] default this.accuracy = (this |> As<Geolocation.Coordinates>).Accuracy
+    [<Inline>] default this.altitudeAccuracy = Some (this |> As<Geolocation.Coordinates>).AltitudeAccuracy
+    [<Inline>] default this.heading = Some (this |> As<Geolocation.Coordinates>).Heading
+    [<Inline>] default this.speed = Some (this |> As<Geolocation.Coordinates>).Speed
 
-// type PositionError =
-//     abstract code: PositionErrorCode
-//     abstract message: string
+[<Proxy(typeof<Browser.Types.Position>)>]
+type PositionProxy =
+    [<Inline>] abstract coords: Browser.Types.Coordinates
+    [<Inline>] abstract timestamp: float
 
-// type PositionOptions =
-//     abstract enableHighAccuracy: bool option with get, set
-//     abstract timeout: int option with get, set
-//     abstract maximumAge: int option with get, set
+    [<Inline>] default this.coords = (this |> As<Geolocation.Position>).Coords |> As
+    [<Inline>] default this.timestamp = (this |> As<Geolocation.Position>).Timestamp |> As
 
-// type [<Global>] Geolocation =
-//     abstract clearWatch: watchId: float -> unit
-//     abstract getCurrentPosition: successCallback: (Position->unit) * ?errorCallback: (PositionError->unit) * ?options: PositionOptions -> unit
-//     abstract watchPosition: successCallback: (Position->unit) * ?errorCallback: (PositionError->unit) * ?options: PositionOptions -> float
+[<Proxy(typeof<Browser.Types.PositionErrorCode>)>]
+type PositionErrorCodeProxy =
+    | PERMISSION_DENIED = 1
+    | POSITION_UNAVAILABLE = 2
+    | TIMEOUT = 3
 
+[<Proxy(typeof<Browser.Types.PositionError>)>]
+type PositionErrorProxy =
+    [<Inline>] abstract code: Browser.Types.PositionErrorCode
+    [<Inline>] abstract message: string
+
+    [<Inline>] default this.code = (this |> As<Geolocation.PositionError>).Code |> As
+    [<Inline>] default this.message = (this |> As<Geolocation.PositionError>).Message
+
+[<Proxy(typeof<Browser.Types.PositionOptions>)>]
+type PositionOptions =
+    [<Inline>] abstract enableHighAccuracy: bool option with get, set
+    [<Inline>] abstract timeout: int option with get, set
+    [<Inline>] abstract maximumAge: int option with get, set
+
+    default this.enableHighAccuracy
+        with [<Inline>]  get () = Some (this |> As<Geolocation.PositionOptions>).EnableHighAccuracy
+        and [<Inline>] set (v) = (this |> As<Geolocation.PositionOptions>).EnableHighAccuracy <- Option.defaultValue false v
+    default this.timeout
+        with [<Inline>] get () = Some (this |> As<Geolocation.PositionOptions>).Timeout
+        and [<Inline>] set (v) = (this |> As<Geolocation.PositionOptions>).Timeout <- Option.defaultValue 0 v
+    default this.maximumAge
+        with [<Inline>] get () = Some (this |> As<Geolocation.PositionOptions>).MaximumAge
+        and [<Inline>] set (v) = (this |> As<Geolocation.PositionOptions>).MaximumAge <- Option.defaultValue 0 v

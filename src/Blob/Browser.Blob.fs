d09ec856cf158a5a70856c9e10324c60a152770c
Gyalus Gergő <gyalus.gergo.98@gmail.com>
2024-01-03 11:57:04 +0100

    Blob re-re-rewrite

--- src/Blob/Browser.Blob.fs
+++ src/Blob/Browser.Blob.fs
@@ -1,31 +1,72 @@
 namespace Browser.Types
 
 open System
+#if FABLE_COMPILER
 open Fable.Core
 open Fable.Core.JS
+#elif JAVASCRIPT
+open WebSharper
+open WebSharper.JavaScript
+#endif
 
-[<StringEnum; RequireQualifiedAccess>]
+type EnumNameAttribute =
+    #if JAVASCRIPT
+    WebSharper.ConstantAttribute
+    #else
+    CompiledNameAttribute
+    #endif
+
+[<RequireQualifiedAccess>]
 type BlobEndings =
     /// Endings are stored in the blob without change
-    | [<CompiledName("transparent")>] Transparent
+    | [<EnumName("transparent")>] Transparent
     /// Line ending characters are changed to match host OS filesystem convention
-    | [<CompiledName("native")>] Native
+    | [<EnumName("native")>] Native
 
 type [<AllowNullLiteral>] BlobPropertyBag =
     abstract ``type``: string with get, set
     abstract endings: BlobEndings with get, set
 
-type [<AllowNullLiteral; Global>] Blob =
-    abstract arrayBuffer: unit -> Promise<ArrayBuffer>
+#if FABLE_COMPILER
+[<Global>]
+#elif JAVASCRIPT
+[<Stub>]
+#endif
+type [<AllowNullLiteral>] Blob =
+    abstract arrayBuffer: unit -> 
+        #if FABLE_COMPILER
+        Promise<ArrayBuffer>
+        #elif JAVASCRIPT
+        WebSharper.JavaScript.Promise<WebSharper.JavaScript.ArrayBuffer>
+        #else
+        unit
+        #endif
     abstract size: int
     abstract ``type``: string
     abstract slice: ?start: int * ?``end``: int * ?contentType: string -> Blob
-    abstract text: unit -> Promise<string>
+    abstract text: unit -> 
+        #if FABLE_COMPILER
+        Promise<string>
+        #elif JAVASCRIPT
+        WebSharper.JavaScript.Promise<string>
+        #else
+        unit
+        #endif
 
 type [<AllowNullLiteral>] BlobType =
-    [<Emit("new $0($1...)")>] abstract Create: ?blobParts: obj[] * ?options: BlobPropertyBag -> Blob
+    #if FABLE_COMPILER
+    [<Emit("new $0($1...)")>] 
+    #elif JAVASCRIPT
+    [<Inline("new Blob($blobParts, $options)")>]
+    #endif
+    abstract Create: ?blobParts: obj[] * ?options: BlobPropertyBag -> Blob
 
-type [<AllowNullLiteral; Global>] FormData =
+#if FABLE_COMPILER
+[<Global>]
+#elif JAVASCRIPT
+[<Stub>]
+#endif
+type [<AllowNullLiteral>] FormData =
     abstract append: name: string * value: string -> unit
     abstract append: name: string * value: Blob * ?filename: string -> unit
     abstract delete: name: string -> unit
@@ -39,14 +80,41 @@ type [<AllowNullLiteral; Global>] FormData =
     abstract values: unit -> obj seq
 
 type [<AllowNullLiteral>] FormDataType =
-    [<Emit("new $0($1...)")>] abstract Create: unit -> FormData
+    #if FABLE_COMPILER
+    [<Emit("new $0($1...)")>] 
+    #elif JAVASCRIPT
+    [<Inline("new FormData()")>]
+    #endif
+    abstract Create: unit -> FormData
 
 namespace Browser
 
+#if FABLE_COMPILER
 open Fable.Core
+#endif
 open Browser.Types
 
 [<AutoOpen>]
 module Blob =
-    let [<Global>] Blob: BlobType = jsNative
-    let [<Global>] FormData: FormDataType = jsNative
+    #if FABLE_COMPILER
+    [<Global>]
+    #elif JAVASCRIPT
+    [<Inline>]
+    #endif
+    let Blob: BlobType = 
+        #if FABLE_COMPILER
+        jsNative
+        #else
+        Unchecked.defaultof<_>
+        #endif
+    #if FABLE_COMPILER
+    [<Global>]
+    #elif JAVASCRIPT
+    [<Inline>]
+    #endif
+    let FormData: FormDataType = 
+        #if FABLE_COMPILER
+        jsNative
+        #else
+        Unchecked.defaultof<_>
+        #endif

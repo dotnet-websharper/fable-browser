4806774b552ecd117942cb14e32043bf510a3b3c
Jooseppi12 <urijozsef@europe.com>
2024-02-28 14:32:03 +0100

    Add EmitAttribute

--- websharper/WebSharper.Fable.Attributes/Attributes.fs
+++ websharper/WebSharper.Fable.Attributes/Attributes.fs
@@ -70,3 +70,88 @@ type EraseAttribute() =
 [<Generated(typeof<NoneGenerator>)>]
 type StringEnumAttribute() =
     inherit System.Attribute()
+
+[<JavaScript(false)>]
+type EmitMacro() =
+    inherit Core.Macro()
+
+    override this.TranslateCall(c) =
+        let arg =
+            match c.Compilation.GetMethodAttributes(c.DefiningType.Entity, c.Method.Entity) with
+            | Some (attr) ->
+                attr
+                |> List.tryFind (fun (attr, args) ->
+                    attr.Value.FullName.Contains "EmitAttribute"
+                )
+                |> Option.bind (fun (attr, args) ->
+                    if args.Length <> 1 then
+                        c.Compilation.AddError(None, "EmitAttribute should be used with a single argument")
+                        None
+                    else
+                        match args.[0] with
+                        | Core.Metadata.ParameterObject.String s ->
+                            Some s
+                        | _ ->
+                            None
+                )
+                |> Option.defaultValue ""
+            | _ -> 
+                c.Compilation.AddError(None, "EmitAttribute should be with a string literal")
+                ""
+        // branch if it's the setter/getter property
+        if arg = "$0[$1]{{=$2}}" then
+            if c.Method.Entity.Value.MethodName.StartsWith "get_" && c.This.IsSome && c.Arguments.Length = 1 then
+                Core.AST.Expression.Verbatim([""; "["; "]"], [c.This.Value; yield! c.Arguments], false)
+                |> Core.MacroOk
+            elif c.Method.Entity.Value.MethodName.StartsWith "set_" && c.This.IsSome && c.Arguments.Length = 2 then
+                Core.AST.Expression.Verbatim([""; "["; "]="; ""], [c.This.Value; yield! c.Arguments], false)
+                |> Core.MacroOk
+            else
+                Core.AST.Expression.Verbatim([""; "["; "]"], [c.This.Value; yield! c.Arguments], false)
+                |> Core.MacroOk
+        else if arg.Contains "{{=$1}}" then
+            let propName = arg.Replace("{{=$1}}", "").Replace("$0", "")
+            if c.Method.Entity.Value.MethodName.StartsWith "get_" && c.This.IsSome && c.Arguments.Length = 0 then
+                Core.AST.Expression.Verbatim([""; propName], [c.This.Value; yield! c.Arguments], false)
+                |> Core.MacroOk
+            elif c.Method.Entity.Value.MethodName.StartsWith "set_" && c.This.IsSome && c.Arguments.Length = 1 then
+                Core.AST.Expression.Verbatim([""; propName + "="; ""], [c.This.Value; yield! c.Arguments], false)
+                |> Core.MacroOk
+            else
+                Core.AST.Expression.Verbatim([""; propName], [c.This.Value; yield! c.Arguments], false)
+                |> Core.MacroOk
+        else
+            // otherwise do the default behavior shit
+
+            let parts,matches =
+                let regex = System.Text.RegularExpressions.Regex("\$\d+(\.\.\.)?")
+                regex.Split(arg),regex.Matches(arg)
+            let mutable needsExtraArgs = 0
+            let v =
+                let enumerator = matches.GetEnumerator()
+                [
+                    while enumerator.MoveNext() do
+                        let i = (enumerator.Current :?> System.Text.RegularExpressions.Match).Value.Trim([|'$'|])
+                        if i.EndsWith "..." then
+                            let i = int (i.TrimEnd([|'.'|]))
+                            if c.This.IsSome then
+                                yield! c.Arguments[i-1 ..]
+                                needsExtraArgs <- c.Arguments[i-1 ..].Length
+                            else
+                                yield! c.Arguments[i ..]
+                                needsExtraArgs <- c.Arguments[i ..].Length
+                        else
+                            let i = int i
+                            if i = 0 && c.This.IsSome then
+                                yield c.This.Value
+                            elif c.This.IsSome then
+                                yield c.Arguments[i-1]
+                            else
+                                yield c.Arguments[i]
+                ]
+            Core.AST.Expression.Verbatim(parts |> List.ofArray |> List.collect (fun x -> if x = "..." && needsExtraArgs >= 1 then List.replicate (needsExtraArgs - 1) "," else [x]), v, false)
+            |> Core.MacroOk
+
+[<Macro(typeof<EmitMacro>)>]
+type EmitAttribute(i: string) =
+    inherit System.Attribute()
